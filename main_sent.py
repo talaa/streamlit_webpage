812f95c57526b8620ef216fa6cc74df1aa6b1be7
talaa <tamer.alaaeldine@gmail.com>
2022-10-02 14:55:25 +0200

    Update main_sent.py

--- main_sent.py
+++ main_sent.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(page_title="News Stock Sentiment",page_icon=:crystal_ball:,layout="wide")
+#Header Section 
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
