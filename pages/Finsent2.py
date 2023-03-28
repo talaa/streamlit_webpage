40cfa0d4ace4f80b3a5efd8faed66fad7ba5e9f3
talaa <tamer.alaaeldine@gmail.com>
2023-03-28 22:07:35 +0000

    Fixing the datestrptime

--- pages/Finsent2.py
+++ pages/Finsent2.py
@@ -32,7 +32,7 @@ for i in range(1, 6):
             source = ""
 
         # Filter articles based on number of days selected
-        date_str = str(result["datetime"])
+        date_str = result["datetime"]
         date_obj = datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S %Z')
         if (datetime.now() - date_obj).days <= days:
             df = pd.concat([df, pd.DataFrame({
