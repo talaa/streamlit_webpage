37e52c98a9c4fd5edbef68a7c9f39d9adc704765
talaa <tamer.alaaeldine@gmail.com>
2023-03-29 08:31:42 +0000

    adjusting the number of Pages + adjusting the date_str

--- pages/Finsent2.py
+++ pages/Finsent2.py
@@ -19,7 +19,7 @@ df = pd.DataFrame(columns=columns)
 googlenews = GoogleNews()
 
 # Loop through each company and get the news articles
-for i in range(1, 6):
+for i in range(1, 4):
     googlenews.search(company + " financial news")
     googlenews.getpage(i)
     results = googlenews.result()
@@ -27,7 +27,9 @@ for i in range(1, 6):
     # Loop through each article and add it to the DataFrame
     for result in results:
         # Filter articles based on number of days selected
-        date_str = result["datetime"]
+        if result["datetime"] is None:
+          continue
+        date_str = result["datetime"].strftime('%Y-%m-%d %H:%M:%S.%f')
         date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
         if (datetime.now() - date_obj).days <= days:
             df = pd.concat([df, pd.DataFrame({
